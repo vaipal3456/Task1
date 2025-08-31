a19a34da5a72650f4b38edb16129280c72a3ce0c
vaipal3456 <vaibhav3456@gmail.com>
2025-08-31 14:35:52 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,23 @@
+#Addition
+a=input('First number: ')
+b=input('Second number: ')
+a=int(a)
+b=int(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print('Addition: ',c)
+print('Substraction: ',d)
+print('Multiplication: ',e)
+print('Division: ',f)
+
+#TASK 2
+x=input('First name: ')
+y=input('Last Name: ' )
+x=str(x )
+y=str(y)
+z=x+y
+print("Hello "+z+"! Welcome to python program")
+
+
