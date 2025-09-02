75d13926e7da210b606cf9f98e5348f1271d79fd
vaipal3456 <vaibhav3456@gmail.com>
2025-09-02 21:57:03 +0530

    Add files via upload

---
+++ Assignmet 2.py
@@ -0,0 +1,16 @@
+num=int(input('Enter your number '))
+if (num // 2)*2==num:
+    print(f"{num}  is an even number")
+else:
+    print(f"{num} is an odd number")
+
+num=int(input('Enter your number '))
+if (num // 2)*2==num:
+    print(f"{num}  is an even number")
+else:
+    print(f"{num} is an odd number")
+
+i=0
+for i in range(1,52):
+  i *= 25
+print(f"The sum of numbers from 1 to 50 is: {i}")
